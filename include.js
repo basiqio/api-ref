4342e22b93442a6aff47a47e237a7acb6d825224
Bridget Turnbull <bridget@192-168-1-108.tpgi.com.au>
2022-03-29 13:48:26 +1100

    Add JS to include in api ref

---
+++ include.js
@@ -0,0 +1,16 @@
+setTimeout(() => {
+    //  const referencePlayground = document.getElementById('ReferencePlayground');
+   //    const referenceChildren = [].slice.call(referencePlayground.children);
+   //    const paramRef = document.querySelector('.param-type');
+   //	  const newParamRef = paramRef.cloneNode(true)
+    //   const newParamRef = document.getElementById('content').removeChild(paramRef);
+     //  const codeSnippet = referenceChildren[3]
+       
+       $( "<p class='toolTip'>&#8505;&#65039; Use the Authentication and Path Params fields to populate your request!</p>" ).insertAfter($('.rm-PlaygroundRequest'))
+       
+       
+   //    paramRef.style.display = "none";
+     //  paramRef.style.height = "0px";
+       
+   //    referencePlayground.insertBefore(newParamRef, codeSnippet);
+       }, 100)
